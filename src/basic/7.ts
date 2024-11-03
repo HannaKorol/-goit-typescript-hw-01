d4002a8dd25d2d1325dbccc48dfa2ffedcbe1142
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-11-03 14:02:14 +0100

    edit solution for 6, add solution for 7 and task details for 1st(generic)

---
+++ src/basic/7.ts
@@ -0,0 +1,45 @@
+//Різниця в type і interface полягає тільки в написанню. Для type потрібно =, а interface одразу записується без цього. 
+//Вказали просто Date, оскільки це вбудований тип для дати в TypeScript.
+
+type IPage = {
+    title: string;
+    likes: number;
+    accounts: string[]; //Array<string> також підходить
+    status: string;
+    details?: {
+        createAt: Date;
+        updateAt: Date;
+    }
+}
+
+
+
+interface IPage {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: string;
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  };
+}
+
+
+const page1 = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
+
+const page2 = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
